99b2b057a993cdb5530abe00c0e1b5fbc252d788
NikitaKrasovskiy <nikitakrasovskij5@gmail.com>
2023-02-22 10:25:18 +0300

    commit letcode days one , succ complite

---
+++ PivotNum.java
@@ -0,0 +1,22 @@
+public class PivotNum {
+    public static void main(String[] args) {
+
+        int[] nums = {1,7,3,6,5,6};
+        System.out.println(pivotIndex(nums));
+    }
+
+    public static int pivotIndex(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length;i++) {
+            sum += nums[i];
+        }
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0) leftSum += nums[i - 1];
+            if (sum - leftSum - nums[i] == leftSum) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
